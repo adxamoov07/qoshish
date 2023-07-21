d5401c6900d94c84d87609b97d2c348a11bbb830
adxamoov07 <adxamovabdulloh06@gmail.com>
2023-07-21 19:20:38 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+let table = document.querySelector("table")
+let banner = document.querySelector(".banner")
+
+function qoshish() {
+    table.style.display = ("none");
+    banner.style.display = ("flex");
+    
+}
+
+function qaytish () {
+    table.style.display = ("block");
+    banner.style.display = ("none");
+}
+
