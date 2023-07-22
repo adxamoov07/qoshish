f84ab5b459dbcfab7fd1080a20250ea62c876903
adxamoov07 <adxamovabdulloh06@gmail.com>
2023-07-22 14:32:26 +0500

    sdsfdg

--- app.js
+++ app.js
@@ -1,14 +1,42 @@
 let table = document.querySelector("table")
 let banner = document.querySelector(".banner")
 
+const email = document.querySelector("#email")
+const Password = document.querySelector("#password")
+const parol = document.querySelector(".parol")
+const navbar = document.querySelector("header")
+
 function qoshish() {
     table.style.display = ("none");
     banner.style.display = ("flex");
+    navbar.style.display = ("none");
+
     
 }
 
 function qaytish () {
     table.style.display = ("block");
     banner.style.display = ("none");
+    navbar.style.display = ("flex");
+
 }
+
+let logineEmail = "hokim"
+let loginePassword = "19825800"
+
+function login1() {
+    if (email.value == logineEmail && password.value == loginePassword) {
+        parol.style.display = ("none");
+        table.style.display = ("block");
+        banner.style.display = ("none");
+        navbar.style.display = ("flex");
+    }
+    else {
+        email.style.borderColor = "red"
+        password.style.borderColor = "red"
+        alert("code xato")
+
+    }
+}
+
 
